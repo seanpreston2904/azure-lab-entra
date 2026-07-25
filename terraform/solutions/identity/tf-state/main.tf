e5570b02342d4989444a741f236f15afd6bf4217
Sean Preston <seanprestondev@gmail.com>
2026-07-25 22:31:35 +1200

    Added service principal

--- terraform/solutions/identity/tf-state/main.tf
+++ terraform/solutions/identity/tf-state/main.tf
@@ -1,13 +1,17 @@
-resource "azuread_application_registration" "sp_id_tf_state" {
+resource "azuread_application_registration" "sp_id_tf_state_app_reg" {
     display_name    = "sp-id-tf-state-management"
     description     = "Used to manage Terraform state for management landing zone."
 }
 
 resource "azuread_application_federated_identity_credential" "sp_id_tf_state_credential" {
-    application_id  = azuread_application_registration.sp_id_tf_state.id
+    application_id  = azuread_application_registration.sp_id_tf_state_app_reg.id
     display_name    = "github-actions"
     description     = "Deploys from azure-lab-entra main branch"
     audiences       = ["api://AzureADTokenExchange"]
     issuer          = "https://token.actions.githubusercontent.com"
     subject         = "repo:seanpreston2904@38393064/azure-lab-entra@1305699783:environment:terraform-deploy"
+}
+
+resource "azuread_service_principal" "sp_id_tf_state_principal" {
+    client_id = azuread_application_registration.sp_id_tf_state_app_reg.client_id
 }
